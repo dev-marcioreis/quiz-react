d0e9588f57dcc0ba00999a12618904078dffa897
Marcio Reis <dev.marcioreis@gmail.com>
2023-04-07 14:03:52 -0300

    media query

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -5,10 +5,10 @@ const Trivia = () => {
     <div className='trivia'>
         <div className="question">Qual o maior Youtuber do mundo?</div>
         <div className="answers">
-            <div className="answer correct">Winderson Nunes</div>
-            <div className="answer">Anita</div>
+            <div className="answer">Winderson Nunes</div>
+            <div className="answer">Anitta</div>
             <div className="answer">Nastia</div>
-            <div className="answer">Mr Beast</div>
+            <div className="answer correct">MrBeast</div>
         </div>
     </div>
   )
